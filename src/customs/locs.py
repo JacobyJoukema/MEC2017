423168072333e714fdeaca69f889b29a2edf6ad4
Or Almog <almogo@mcmaster.ca>
2017-10-21 18:28:59 -0400

    autocommit

--- src/customs/locs.py
+++ src/customs/locs.py
@@ -9,7 +9,7 @@ class Crop:
 		self.loc = [0,0]
 		self.mag = 0
 
-def get_farms():
+def read_farms():
 	f = open("farms", "r")
 	farms = [i.split() for i in f.readlines()]
 	f.close()
@@ -17,7 +17,21 @@ def get_farms():
 		i[0] = i[0].replace("_", " ")
 	return farms
 
-a = get_farms()
+def add_farm(farms, name, x ,y):
+	farms.append([name, x, y])
+
+def save_farms(farms):
+	f = open("farms", "w")
+	for fa in farms:
+		f.write(fa[0].replace(" ", "_") + " " + str(fa[1]) + " " + str(fa[2]) + "\n")
+	f.close()
+
+a = read_farms()
 for i in a:
 	print i
 
+add_farm(a, "My Happy Place", 1, 1)
+for i in a:
+	print i
+
+save_farms(a)
