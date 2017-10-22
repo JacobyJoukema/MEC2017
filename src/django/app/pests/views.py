57e172e03fce84fc750efaa186e1bfd89a68b5bd
Sigma-Squared <chamu.rajasekera@gmail.com>
2017-10-21 21:06:18 -0400

    things

--- src/django/app/pests/views.py
+++ src/django/app/pests/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.http import JsonResponse
+import random
+
+i = 0
+def get_i():
+    global i
+    i += 1
+    return i
 
 class MainView(TemplateView):
     template_name = 'pests/index.html'
+
+class PointApiView(View):
+    i = 0
+    def get(self, request):
+        points = []
+        latmin = 43.076149
+        latmax = 43.168864
+        longmin = -80.125481
+        longmax = -79.930081
+        self.i += 1
+        return JsonResponse({
+            'points': get_i(),
+        })
 
 # Create your views here.
--- src/django/app/urls.py
+++ src/django/app/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from .pests.views import MainView
+from .pests.views import MainView, PointApiView
 
 urlpatterns = [
     url(r'^$', MainView.as_view()),
+    url(r'^api/point', PointApiView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
